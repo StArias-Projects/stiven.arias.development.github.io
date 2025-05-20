6243f9c7346a8271119843a17fc4fb9b5334e163
starias <starias2797@gmail.com>
2025-05-20 18:28:34 +0300

    ADD: changes in the react template

---
+++ src/components/About.js
@@ -0,0 +1,24 @@
+function About() {
+  return (
+    <section id="about">
+        <h2>About</h2> 
+        <p>
+          I'm a freelancer, and you can reach out to me on <a href="https://www.upwork.com/en-gb/freelancers/~018f7e0e1df08439dd" target="_blank" rel="noopener noreferrer">Upwork</a>  
+          or contact me directly. Feel free to check my profiles on different platforms.
+          <br></br>
+          I hold a degree in Video Games Development from Universidad Complutense of Madrid. 
+          I have demonstrable professional experience in the industry, and have worked on several personal projects.
+          <br></br>
+          In both professional and academic fields, I have a strong track record of developing high-quality code to solve complex tasks and challenges.
+          In addition, I excel at working in teams, taking initiative, and demonstrating enthusiasm, leadership, and strong social skills.
+          <br></br>
+          ✨Check out my work below!✨
+          <br></br>
+        </p>
+        <li><a href="#experience">Go to Experience</a></li>
+        <li><a href="#personal-projects">Go to Projects</a></li>
+    </section>
+  );
+}
+
+export default About;
---
+++ src/components/ReactDefault.js
@@ -0,0 +1,15 @@
+
+import logo from '../assets/icons/starias-icon.png';
+
+function ReactDefault() {
+  return (
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Stiven Arias Giraldo
+        </p>
+      </header>
+  );
+}
+
+export default ReactDefault;
